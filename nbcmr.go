a17f8245d936b8e4905ecfc53b69f68518530370
konstantinos <kostas@norbloc.com>
2024-08-14 16:10:10 +0300

    Run both loops with gofunc

--- nbcmr.go
+++ nbcmr.go
@@ -116,31 +116,35 @@ func (r *nbcmrReceiver) Start(ctx context.Context, host component.Host) error {
 		log.Println("HTTP Ticker created")
 	
 		// Run the code in a loop with a ticker
-		log.Println("Starting loop")
-		for range ticker.C {
-			log.Println("Listing selected ConfigMaps:")
-			for name, namespace := range configMapMap {
-				log.Printf("Getting ConfigMap %s in namespace %s", name, namespace)
-				configmap, err := clientset.CoreV1().ConfigMaps(namespace).Get(context.TODO(), name, metav1.GetOptions{})
-				if err != nil {
-					log.Printf("Error getting ConfigMap %s in namespace %s: %s", name, namespace, err.Error())
-					continue
-				}
-				if configmap != nil {
-					log.Printf("Namespace: %s, Name: %s, Data: %v", configmap.Namespace, configmap.Name, configmap.Data)
+		go func() {
+			log.Println("Starting loop")
+			for range ticker.C {
+				log.Println("Listing selected ConfigMaps:")
+				for name, namespace := range configMapMap {
+					log.Printf("Getting ConfigMap %s in namespace %s", name, namespace)
+					configmap, err := clientset.CoreV1().ConfigMaps(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+					if err != nil {
+						log.Printf("Error getting ConfigMap %s in namespace %s: %s", name, namespace, err.Error())
+						continue
+					}
+					if configmap != nil {
+						log.Printf("Namespace: %s, Name: %s, Data: %v", configmap.Namespace, configmap.Name, configmap.Data)
+					}
 				}
 			}
-		}
-		for range httpticker.C {
+		}()
+		go func() {
+			for range httpticker.C {
 			log.Println("Checking http connection...")
 			conn, err := net.DialTimeout("tcp", "www.google.com:80", 3*time.Second)
 			if err != nil {
 				fmt.Println("port closed")
-				return err
+			} else {
+				fmt.Println("port open")
 			}
-			defer conn.Close()
-			fmt.Println("port open")
+			conn.Close()
 		}
+		}()
 		return nil
 	}
 
